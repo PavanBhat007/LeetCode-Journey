2c7d9f8daa84853d61f870218451b117fd0a66a2
Pavan Bhat <pavanksp612@gmail.com>
2024-01-21 14:23:48 +0530

    Day 6 - Valid Parantheses [EASY]

--- 20-ValidParantheses.py
+++ 20-ValidParantheses.py
@@ -1,3 +1,17 @@
+"""
+20. Valid Parentheses [Easy]
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
 class ValidParantheses:
     def __init__(self):
         self.stack = []
