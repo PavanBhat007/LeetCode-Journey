d2469e83b3892785167b8d1bc8a61fd06501bed7
Pavan Bhat <pavanksp612@gmail.com>
2024-01-25 13:38:53 +0530

    Day 7 - Remove Duplicates from Sorted Array [EASY], Remove Element[EASY]

--- 27-RemEle.py
+++ 27-RemEle.py
@@ -1,3 +1,18 @@
+"""
+27. Remove Element [Easy]
+
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+"""
+
+# ------------------------------------------------------------------------------------------------------------------
+
 def removeElement(nums: list[int], val: int) -> list:
   i = 0
   limit = len(nums)
