b34b6a72f5a057bde24bf927eec290e00bb9afd5
Pavan Bhat <pavanksp612@gmail.com>
2024-07-23 23:12:32 +0530

    C++ Day 4 : Maximum Subarray (Follow-Up qstn)

--- 53-MaximumSubarray.cpp
+++ 53-MaximumSubarray.cpp
@@ -22,16 +22,44 @@ int maxSubArray(vector<int>& nums) {
     return max;
 }
 
+// Follow Up: Print max subarray
+void printMaxSubarray(vector<int>& nums) {
+    int max = INT_MIN, sum = 0;
+    int start = 0, ansStart = -1, ansEnd = -1; // extra
+
+    for(int i=0; i<nums.size(); i++) {
+        // everytime sum = 0 re-initialized => new subarray
+        if(sum == 0) start = i;
+        sum += nums[i];
+        
+        // max subarray found = update indices
+        if(sum > max) {
+            max = sum;
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        if(sum < 0) sum = 0;
+    }
+
+    cout << "MAX SUBARRAY: [ ";
+    for(int i=ansStart; i<ansEnd; i++)
+        cout << nums[i] << " ";
+
+    cout << "]" << endl;
+}
+
 int main() {
     int n;
     cout << "SIZE OF ARRAY: ";
     cin >> n;
 
-    vector <int> arr (n);
+    vector<int> arr(n);
     cout << "ARRAY: ";
     for(int i=0; i<arr.size(); i++)
         cin >> arr[i];
 
+    printMaxSubarray(arr);
     cout << "MAX SUBARRAY SUM: " << maxSubArray(arr) << endl;
     return 0;
 }
